b593e57f960b02cb03905cce6f5c94f6403485f3
Deepak Kumar <deepakbhardwa0805@.com>
2023-02-26 16:09:43 +0530

    data

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-# theServiceBackend
-
-1). npm init -y
-2). create server.js and paste =>{`const jsonServer = require('json-server')
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -10,8 +7,4 @@ server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
-})`}
-
-3). npm i json-server cors
-4). npm i json-serve
-5). create db.json and paste your data
+})
